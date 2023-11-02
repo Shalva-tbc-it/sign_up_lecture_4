2e394862f7dacc3c14435a3b8b17aa49b17b7a0f
shalva96 <gmshalva@gmail.com>
2023-11-03 01:45:22 +0400

    add shadow

--- app/src/main/java/com/example/sugnup/MainActivity.kt
+++ app/src/main/java/com/example/sugnup/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.sugnup
 
+import android.app.Activity
+import android.app.Person
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,11 +10,11 @@ import com.example.sugnup.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
     }
 }
